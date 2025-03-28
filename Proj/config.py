4e4fee3d3e736c1eacbfaeb9a62c6bb1f5258446
AFewAngry <155112169+AFewAngry@users.noreply.github.com>
2025-03-28 16:50:04 +0700

    Update config.py

--- Proj/config.py
+++ Proj/config.py
@@ -1,5 +1,3 @@
-# TOKEN = "7802784921:AAHjeDU2Cp_THJGEnDpsvE5sM67L3qxQioQ/+--------------------------------------------------------------------------------------------*****************
-
-TOKEN = "8022922442:AAGQvRbBaW8iIgskBZmGyYPjxjs6JZ_gYQ0"
-ADMINS = [1873761933]
+TOKEN = ""
+ADMINS = []
 db_name = "database.db"
